19f9fb5cfab6fad23438cc4ea537659163f3dd58
Stewart DevTeam <stewartdevteam@gmail.com>
2026-04-03 14:08:15 -0500

    Simplificaciones

--- assets/character/character.gd
+++ assets/character/character.gd
@@ -1,1 +1,13 @@
 class_name Character extends CharacterBody2D
+
+
+@export_group("Movement")
+## Velocidad al caminar
+@export var walk_speed: float = 12.0
+## Velocidad al correr
+@export var run_speed: float = 24.0
+
+
+@export_group("Cámara")
+## Velocidad a la que la cámara sigue al jugador
+@export var follow_speed := 5.0
--- assets/character/state_machine/walk.gd
+++ assets/character/state_machine/walk.gd
@@ -1,9 +1,6 @@
 class_name CharacterWalk extends CharacterState
 
 
-@export var walk_speed: float = 12.0
-
-
 func physics_update(_delta: float) -> void:
 	# Si está quieto, pasa a Idle
 	if not direction: to_state.emit(CharacterIdle)
@@ -11,5 +8,5 @@ func physics_update(_delta: float) -> void:
 	# Si presionamos correr, pasamos a Run
 	if Input.is_action_pressed("sprint"): to_state.emit(CharacterRun)
 
-	character.velocity = direction * walk_speed
+	character.velocity = direction * character.walk_speed
 	character.move_and_slide()
