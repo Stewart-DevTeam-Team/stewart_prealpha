7da61650d59e250549e683e38533f4dc6d207d9a
Andrés López <andreslopez01182009@gmail.com>
2026-05-09 18:39:15 -0500

    Conexión con el menú principal

--- assets/ui/start_menu/start_menu.gd
+++ assets/ui/start_menu/start_menu.gd
@@ -2,7 +2,7 @@ extends Control
 
 
 func _on_play_button_pressed() -> void:
-	pass # Replace with function body.
+	SceneManager.change_to_scene("world")
 
 
 func _on_credits_button_pressed() -> void:
